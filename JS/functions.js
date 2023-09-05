27938f3b2f35b6ff5b90b3d44c3d6362598bc9f3
Rafael Gabardo <rggabardo@gmail.com>
2023-09-05 17:08:19 -0300

    Recomeçando

---
+++ JS/functions.js
@@ -0,0 +1,23 @@
+function whereTo() {
+    switch (map) {
+        case 0:
+            alert('Isso é uma parede, seu cego!')
+            break;
+        case 1:
+            alert('Você segue por um corredor!')
+
+        default:
+            break;
+    }
+}
+
+function changeImage() {
+    switch (map) {
+        case 0:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
---
+++ JS/variables.js
@@ -0,0 +1,33 @@
+const map = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [6, 9, 1, 1, 1, 3, 1, 1, 1, 1, 0],
+    [0, 0, 0, 2, 0, 5, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+    [0, 1, 4, 0, 0, 0, 0, 0, 2, 5, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 7, 8],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+const hero = {
+    name: '',
+    life: 100,
+    atk: 1,
+    def: 0,
+    inventory: {
+        heal: 2,
+        weapon: null,
+        armor: null,
+        torch: 1
+    }
+}
+
+const images = {
+    begin: '../Imagens/imagem-floresta-inicio.jpeg',
+    entrance: '../Imagens/imagem-entrada-caverna.jpg',
+    corridor: '../Imagens/imagem-corredor-caverna.jpg',
+    armor: '../Imagens/imagem-armadura.jpg',
+    weapon: '../Imagens/imagem-area-aberta-caverna.jpg',
+    monster: '../Imagens/imagem-monstro-caverna.jpg',
+    boss: '../Imagens/imagem-boss-caverna.jpg',
+    friends: '../Imagens/imagem-final-caverna.jpg'
+}
